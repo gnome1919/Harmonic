00904176458635a3a636eab8f93daf309f8c218a
Farrokh A. Ghavanini <7593861+gnome1919@users.noreply.github.com>
2022-04-13 12:52:08 +0430

    Add files via upload

---
+++ Harmonic/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Harmonic.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace Harmonic
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+		private void NoOfInfTbx_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+		{
+			var numberOfRows = Convert.ToInt32(e.Text);
+
+			List<TicketInfo> ticketsList = new List<TicketInfo>();
+
+			for (int i = 1; i < numberOfRows; i++)
+			{
+				ticketsList.Add(new TicketInfo());
+			}
+			
+			dgData.ItemsSource = ticketsList;
+		}
+	}
+
+	public class Para1_List : List<string>
+    {
+        public Para1_List()
+        {
+            for (int i = 1; i < 147; i++)
+            {
+                this.Add("P1");
+                this.Add("P2");
+                this.Add("P3");
+                this.Add("P4");
+                this.Add("P5");
+            }
+        }
+    }
+    public class Para2_List : List<string>
+    {
+        public Para2_List()
+        {
+            this.Add("P6");
+            this.Add("P7");
+            this.Add("P8");
+            this.Add("P9");
+            this.Add("P10");
+        }
+    }
+}
---
+++ Harmonic/Models/TicketInfo.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+
+namespace Harmonic.Models
+{
+	public class TicketInfo : INotifyPropertyChanged
+	{
+		private string period;
+		private string period2;
+
+		public string Period { get => period; set { period = value; OnPropertyChanged(nameof(Calc)); } }
+		public string Period2 { get => period2; set { period2 = value; OnPropertyChanged(nameof(Calc2)); } }
+		public string Calc
+		{
+			get
+			{
+				switch (period)
+				{
+					case "P1":
+						return "Period_1_1";
+					case "P2":
+						return "Period_1_2";
+					case "P3":		   
+						return "Period_1_3";
+					case "P4":		   
+						return "Period_1_4";
+					case "P5":		   
+						return "Period_1_5";
+					default:
+						return "";
+				}
+			}
+		}
+
+		public string Calc2
+		{
+			get
+			{
+				switch (period2)
+				{
+					case "P6":
+						return "Period_2_1";
+					case "P7":		   
+						return "Period_2_2";
+					case "P8":		   
+						return "Period_2_3";
+					case "P9":		   
+						return "Period_2_4";
+					case "P10":		   
+						return "Period_2_5";
+					default:
+						return "";
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
